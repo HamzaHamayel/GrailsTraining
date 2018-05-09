abe66fdcf544a7123b4c4155b2c3781bc8b90378
Hamza Hamayel <hamza.hamayel@gmail.com>
2018-05-09 19:06:18 +0300

    --init

--- grails-app/controllers/grailstraining/UrlMappings.groovy
+++ grails-app/controllers/grailstraining/UrlMappings.groovy
@@ -9,9 +9,25 @@ class UrlMappings {
             }
         }
 
-//        "/myPanel/list/$id?"(controller: "user", action: "list")
-//        "/myPanel/show/$id?"(controller: "user", action: "show")
 
+        "/users"(controller: "user",action: "list")
+
+        "/myPanel/view/$id?" {
+            controller = "user"
+            action = "show"
+
+            constraints {
+                // apply constraints here
+                id(validator: {
+                    if (it == "21") {
+                        return false
+                    }else{
+                        return true
+                    }
+                })
+            }
+
+        }
 
         "/"(view:"/index")
         "500"(view:'/error')
